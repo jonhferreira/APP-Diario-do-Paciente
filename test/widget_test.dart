51d0f050df368443207a48eb2df920fb0027c83c
Ayrton Fernandes Hora <61425939+ayrton-hora@users.noreply.github.com>
2021-07-11 16:13:57 -0300

    Add files via upload

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,8 +8,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:helloworld/main.dart';
-import 'package:helloworld/app.dart';
+import 'package:diario_do_paciente/example.dart';
+
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\ayrto\\AppData\\Local\\Programs\\Flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "G:\\Meu Drive\\6° Período\\Engenharia de Software II\\Projeto\\Code\\diario_do_paciente" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\ayrto\\AppData\\Local\\Programs\\Flutter"
+  "PROJECT_DIR=G:\\Meu Drive\\6° Período\\Engenharia de Software II\\Projeto\\Code\\diario_do_paciente"
+  "FLUTTER_ROOT=C:\\Users\\ayrto\\AppData\\Local\\Programs\\Flutter"
+  "FLUTTER_EPHEMERAL_DIR=G:\\Meu Drive\\6° Período\\Engenharia de Software II\\Projeto\\Code\\diario_do_paciente\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=G:\\Meu Drive\\6° Período\\Engenharia de Software II\\Projeto\\Code\\diario_do_paciente"
+  "FLUTTER_TARGET=G:\\Meu Drive\\6° Período\\Engenharia de Software II\\Projeto\\Code\\diario_do_paciente\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=G:\\Meu Drive\\6° Período\\Engenharia de Software II\\Projeto\\Code\\diario_do_paciente\\.dart_tool\\package_config.json"
+)
